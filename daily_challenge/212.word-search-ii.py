d07534754125d67d3daa8c8d8f597115ca2639b3
blacktr1007 <ohs1192kou@gmail.com>
2022-11-05 17:49:59 +0900

    solve (cheated): 2022-11-05

--- daily_challenge/212.word-search-ii.py
+++ daily_challenge/212.word-search-ii.py
@@ -5,27 +5,42 @@
 #
 
 # @lc code=start
+from functools import reduce
+from collections import defaultdict
 class Solution:
     def findWords(self, board: list[list[str]], words: list[str]) -> list[str]:
-        c_map: dict[str, list[tuple[int, int]]] = dict()
-        exists: list[str] = []
+        # create trie
+        Trie = lambda: defaultdict(Trie)
+        trie = Trie()
+        END = True
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if (c := board[i][j]) in c_map:
-                    c_map[c].append((i, j))
-                else:
-                    c_map[c] = [(i, j)]
+        for word in words:
+            reduce(dict.__getitem__,word,trie)[END] = word
 
-        for w in words:
-            for c in w:
-                if c not in c_map:
-                    break
+        res = set()
+        def findstr(i,j,t):
+            if END in t:
+                res.add(t[END])
+                # return
+            letter = board[i][j]
+            board[i][j] = ""
+            if i > 0 and board[i-1][j] in t:
+                findstr(i-1,j,t[board[i-1][j]])
+            if j>0 and board[i][j-1] in t:
+                findstr(i,j-1,t[board[i][j-1]])
+            if i < len(board)-1 and board[i+1][j] in t:
+                findstr(i+1,j,t[board[i+1][j]])
+            if j < len(board[0])-1 and board[i][j+1] in t:
+                findstr(i,j+1,t[board[i][j+1]])
+            board[i][j] = letter
 
-                for p in c_map[c]:
-                    
+            return 
 
-        return exists
+        for i, row in enumerate(board):
+            for j, char in enumerate(row):
+                if board[i][j] in trie:
+                    findstr(i,j,trie[board[i][j]])
+        return res
 
 # @lc code=end
 
